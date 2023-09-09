637a428d82270a4e6cab3a7cf755c15d873ec035
juliusnjeru <juliusn411@gmail.com>
2023-09-09 19:15:40 +0300

    The project is not yet done. But its almost there.
    laith's tutorial has some issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Home from "./pages/Home";
+import SearchAppBar from "./components/AppBar";
+import cities from "./data.json";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <SearchAppBar />
+            <Container sx={{ marginTop: "2rem" }}>
+                {cities.map((city, index) => (
+                    <>
+                        <Typography
+                            sx={{ marginTop: "2rem", marginBottom: "1rem" }}
+                            variant="h4"
+                            component="h2"
+                        >
+                            Top {city.name} tours
+                        </Typography>
+                        <Grid container spacing={5}>
+                            {city.tours.map((dest, index) => (
+                                <ToursCard {...dest} key={index} />
+                            ))}
+                        </Grid>
+                    </>
+                ))}
+            </Container>
+        </>
+    );
 }
 
-export default App
+export default App;
